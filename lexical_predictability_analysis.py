1404f88ac197cc74ad5fdcdbc8f72891f2221993
clelf <44549540+clelf@users.noreply.github.com>
2024-05-24 16:22:17 +0200

    adjust tqdm

--- lexical_predictability_analysis.py
+++ lexical_predictability_analysis.py
@@ -46,7 +46,7 @@ def word_by_word_predictability(model, tokenizer, text_sample, sample_id, level)
     preds = []
 
     # Test word predictability for every word in sample one by one
-    for word_id, word in tqdm(enumerate(encoded_input_ids)):
+    for word_id, word in tqdm(enumerate(encoded_input_ids), total=len(encoded_input_ids)):
         # Start at second word, to have at least 1 previous word of context
         if word_id == 0: continue
 
@@ -87,7 +87,7 @@ def lexical_predictability_analysis(data_path, compare_original=False):
     for level in tqdm(data.disorder_level.unique()):
         data_level = data[data['disorder_level'] == level]
 
-        for sample_id, sample in tqdm(data_level.iterrows()):
+        for sample_id, sample in tqdm(data_level.iterrows(), total=len(data_level)):
             # Get word-by-word predictability scores, varying context length for each word, for given sample
             preds_sample = word_by_word_predictability(model, tokenizer, sample['text_shuffled'], sample_id, level)
             preds.extend(preds_sample)
