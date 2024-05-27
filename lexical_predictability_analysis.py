5630d48788b50e6e94843abac6e9617a0b5eb5bc
clelf <44549540+clelf@users.noreply.github.com>
2024-05-27 16:39:55 +0200

    try different disorder levels

--- lexical_predictability_analysis.py
+++ lexical_predictability_analysis.py
@@ -153,10 +153,10 @@ def visualize_predictability(df_preds):
 
 
 if __name__ == '__main__':
-    compute_pred=False
+    compute_pred=True
     if compute_pred:
         df_preds = lexical_predictability_analysis(data_path="text_samples_trunc_gpt2tokenfast")
         df_preds.to_csv("pred_scores_1sample_10levels.csv", index=False)
     else:
         df_preds = pd.read_csv("pred_scores_1samples.csv", index_col=[0])
-    visualize_predictability(visualize_predictability(df_preds))
+    # visualize_predictability(visualize_predictability(df_preds))
